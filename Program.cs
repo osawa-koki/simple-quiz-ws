1797a7a44ad02e15e0fbf0ee5b11222d59ffa151
osawa-koki <osawa-koki@github.com>
2022-10-22 11:07:52 +0000

    CORS対策。

--- Program.cs
+++ Program.cs
@@ -5,12 +5,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddSignalR();
 
+builder.Services.AddCors(options =>
+{
+  options.AddDefaultPolicy(
+    builder =>
+    {
+      builder.WithOrigins("*")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    }
+  );
+});
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
+  app.UseExceptionHandler("/Error");
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -19,6 +31,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+// UseCors must be called before MapHub.
+app.UseCors();
 
 app.MapRazorPages();
 app.MapHub<DebugHub>("/debug");
